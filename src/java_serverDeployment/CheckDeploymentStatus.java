a4e88cae60517b93ecad318fb1d1e43c5d484dea
P Ravishankar <p.ravishankar@mphasis.com>
2021-02-01 18:40:09 +0530

    tomcat deployment status

---
+++ src/java_serverDeployment/CheckDeploymentStatus.java
@@ -0,0 +1,8 @@
+package java_serverDeployment;
+
+public class CheckDeploymentStatus {
+public static void main(String[] args) {
+	
+	System.out.println("Successfully Deployed to Tomcat Server....");
+}
+}
